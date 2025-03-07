f66689b978336e39492ee8798450c626848e6d7f
Harivandana-jampani <harivandana.jampani>
2025-03-07 08:22:30 +0530

    06-mar-2015 Getter and setter,iterators

---
+++ force-app/main/default/lwc/exploreIterators/exploreIterators.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class ExploreIterators extends LightningElement {
+
+    cars=[
+        {
+            id:1,
+            name:'BMW',
+            color:'black'
+        },
+        {
+            id:2,
+            name:'Audi',
+            color:'red' 
+        },
+        {
+            id:3,
+            name:'volvo',
+            color:'blue'
+        }
+    ];
+}
