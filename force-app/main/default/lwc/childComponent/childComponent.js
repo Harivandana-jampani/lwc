5811730797641fdd55df642579ebb831289c8ac5
Harivandana-jampani <harivandana.jampani>
2025-03-10 14:28:46 +0530

    events 3-10-2015

---
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class ChildComponent extends LightningElement {
+
+    handleClick(){
+        
+         this.dispatchEvent(new CustomEvent('rdata',{detail:"hey!! salesforce"}));
+    }
+}
