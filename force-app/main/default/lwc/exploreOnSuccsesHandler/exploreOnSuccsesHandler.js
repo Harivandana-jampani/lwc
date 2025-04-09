01eb4dd27df7aa4aea833dc9faae943005a5735f
Harivandana-jampani <harivandana.jampani>
2025-04-09 19:24:57 +0530

    onSubmit,onSuccess,onError 09-04-25

--- force-app/main/default/lwc/exploreOnSuccsesHandler/exploreOnSuccsesHandler.js
+++ force-app/main/default/lwc/exploreOnSuccsesHandler/exploreOnSuccsesHandler.js
@@ -2,6 +2,13 @@ import { api, LightningElement } from 'lwc';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 export default class ExploreOnSuccsesHandler extends LightningElement {
     @ api recordId;
+
+    handleSubmit(event){
+        event.preventDefault();
+        const field= event.detail.fields;
+        field.Name="hello";
+        this.template.querySelector('lightning-record-edit-form').submit(field);
+    }
     handleSuccess(event){
         console.log(event);
         const msg = new ShowToastEvent({
