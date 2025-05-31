0fc0daca8bf5da13a6c1728007a049a8305d1ba7
Harivandana-jampani <harivandana.jampani>
2025-05-31 20:39:51 +0530

    31-05-2025 custom labels,static access resources ,user info,naviagtion

---
+++ force-app/main/default/lwc/accessStaticResources/accessStaticResources.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import Daddy from '@salesforce/resourceUrl/Daddy';
+export default class AccessStaticResources extends LightningElement {
+    DadImg = Daddy;
+}
