079052f8883d5956c77086478b7da7dd4a36ff5e
Harivandana-jampani <harivandana.jampani>
2025-06-30 12:07:07 +0530

    pagination

---
+++ force-app/main/default/lwc/parentComponentEvent/parentComponentEvent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+export default class ParentComponentEvent extends LightningElement {
+    message;
+    handleCustomEvent(event){
+        const childMessage = event.detail;
+        this.message = childMessage;
+    }
+
+}
